909e03a6f3e85764ee940a3168a15f6a1f9cbb92
Joselyne Carranza <josy_carranzat@hotmail.com>
2020-02-13 09:18:51 -0600

    Actualizar

--- Clases/Script_5.R
+++ Clases/Script_5.R
@@ -8,8 +8,77 @@ conjunto <- source_data("https://www.dropbox.com/s/hmsf07bbayxv6m3/cuadro1.csv?d
 
 shapiro.test(conjunto$Altura)
 
+shapiro.test(conjunto$Diametro)
+hist(conjunto$Diametro)
+
+boxplot(conjunto$Diametro ~ conjunto$Especie, col= "grey")
+
+boxplot(conjunto$Vecinos ~ conjunto$Especie, col= "grey")
+
+boxplot(conjunto$Altura ~ conjunto$Especie, col= "grey")
+
+frecu <-table(conjunto$Clase)
+prop.table(frecu)
+hist(frecu)
+
+boxplot(conjunto$Clase ~ conjunto$Especie)
+
+var(conjunto$Diametro)
+var(conjunto$Altura)
+
+
+
+
+# Datos ebanos ------------------------------------------------------------
+
+Ebano <- read.csv("Clases/ebanos2.csv", header = T)
+
+hist(Ebano$diametro)
+hist(Ebano$altura)
+
+boxplot(Ebano$diametro)
+boxplot(Ebano$altura)
+
+
+
+fivenum(Ebano$diametro)
+
+
+# ¿Cuántos individuos tienen un Diam menor al 1er quartil? ----------------
+
+
+sum(Ebano$diametro <= 15.55)
+
+
+# ¿Cuántos individuos tienen un Diam menor al 3er quartil? ----------------
+
+sum(Ebano$diametro <= 34.25)
+
+# ¿Cuantos individuos tienen un diametro mayor al 1er quartil
+# pero menor o igual al 2do quartil?
+
+
+sum(Ebano$diametro >= 15.55 & Ebano$diametro <= 25.90)
+
+
+sum(Ebano$diametro <= 25.9)
+
+
+# ¿Cuantos individuos tienen un diametro mayor al 1er quartil
+# pero menor o igual al 3er quartil?
+
+
+sum(Ebano$diametro >= 10 & Ebano$diametro <= 15.55)
+
+
+
+
+
+
 ks.test(conjunto$Diametro, "pnorm", 
         mean=mean(conjunto$Diametro, 
                   sd=sd(conjunto$Diametro)))
 
-hist(conjunto$Diametro)
+
+
+
